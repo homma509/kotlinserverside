e476a71c5e4a53a1c061df84d8a59078cf234dd9
homma509 <kouki.honma@gmail.com>
2025-02-09 09:10:16 +0900

    feat: Spring Initilizar でひな型を作成

---
+++ src/main/kotlin/com/github/homma509/kotlinserverside/KotlinserversideApplication.kt
@@ -0,0 +1,11 @@
+package com.github.homma509.kotlinserverside
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinserversideApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinserversideApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/homma509/kotlinserverside/KotlinserversideApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.homma509.kotlinserverside
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinserversideApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
