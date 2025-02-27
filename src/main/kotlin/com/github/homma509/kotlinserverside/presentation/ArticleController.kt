a4018be4046607b16c26692388ad95670168cab5
homma509 <kouki.honma@gmail.com>
2025-02-28 05:58:44 +0900

    feat: プレゼンテーション層の記事コントローラーの追加

---
+++ src/main/kotlin/com/github/homma509/kotlinserverside/presentation/ArticleController.kt
@@ -0,0 +1,14 @@
+package com.github.homma509.kotlinserverside.presentation
+
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * 作成済記事のコントローラ
+ *
+ */
+@RestController
+@Validated
+class ArticleController {
+    //
+}
