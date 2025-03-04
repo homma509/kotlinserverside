fc454ac7f55b654f4ea23dcc16f2bb092c31e319
homma509 <kouki.honma@gmail.com>
2025-03-05 05:50:53 +0900

    feat: SpringDoc を生成するアノテーションを記述

--- src/main/kotlin/com/github/homma509/kotlinserverside/presentation/ArticleController.kt
+++ src/main/kotlin/com/github/homma509/kotlinserverside/presentation/ArticleController.kt
@@ -6,8 +6,12 @@ import com.github.homma509.kotlinserverside.presentation.model.GenericErrorModel
 import com.github.homma509.kotlinserverside.presentation.model.GenericErrorModelErrors
 import com.github.homma509.kotlinserverside.presentation.model.SingleArticleResponse
 import com.github.homma509.kotlinserverside.usecase.ShowArticleUseCase
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import jakarta.validation.Valid
 import org.hibernate.validator.constraints.Length
 import org.springframework.http.HttpStatus
@@ -32,6 +36,61 @@ class ArticleController(val showArticleUseCase: ShowArticleUseCase) {
      *
      * @return
      */
+    @Operation(
+        summary = "単一記事取得",
+        operationId = "getArticle",
+        description = "slug に一致する記事を取得します。",
+        tags = ["articles"],
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "OK",
+                content = [
+                    Content(
+                        schema = Schema(implementation = SingleArticleResponse::class),
+                        examples = [
+                            ExampleObject(
+                                name = "OK",
+                                value = """
+                                    {
+                                        "article": {
+                                            "slug": "283e60096c26aa3a39cf04712cdd1ff7",
+                                            "title": "title",
+                                            "description": "description",
+                                            "body": "body"
+                                        }
+                                    }
+                                """
+                            )
+                        ]
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Not Found",
+                content = [
+                    Content(
+                        schema = Schema(implementation = GenericErrorModel::class),
+                        examples = [
+                            ExampleObject(
+                                name = "Not Found",
+                                value = """
+                                    {
+                                        "errors": {
+                                            "body": [
+                                                "slug に該当する記事は見つかりませんでした"
+                                            ]
+                                        }
+                                    }
+                                """
+                            )
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
     @GetMapping("/api/articles/{slug}", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getArticle(
         @Parameter(description = "記事の slug", required = true, schema = Schema(minLength = 32, maxLength = 32)) @Valid @PathVariable("slug") @Length(min = 32, max = 32) slug: String,
