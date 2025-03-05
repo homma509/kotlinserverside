4766977fd192b09ac5c6a6c82ee021b69e4f9162
homma509 <kouki.honma@gmail.com>
2025-03-06 06:21:02 +0900

    feat: 作成記事リポジトリのインターフェースの配置および具象クラスの配置

---
+++ src/main/kotlin/com/github/homma509/kotlinserverside/domain/ArticleRepository.kt
@@ -0,0 +1,23 @@
+package com.github.homma509.kotlinserverside.domain
+
+import arrow.core.Either
+
+/**
+ * 作成済記事リポジトリ
+ *
+ */
+interface ArticleRepository {
+    /**
+     * slug から作成済記事を取得
+     *
+     * @param slug
+     * @return
+     */
+    fun findBySlug(slug: Slug): Either<FindBySlugError, CreatedArticle> = throw NotImplementedError()
+
+    /**
+     * 作成済記事取得時のエラー
+     *
+     */
+    sealed interface FindBySlugError
+}
---
+++ src/main/kotlin/com/github/homma509/kotlinserverside/infra/ArticleRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.github.homma509.kotlinserverside.infra
+
+import com.github.homma509.kotlinserverside.domain.ArticleRepository
+import org.springframework.stereotype.Repository
+
+/**
+ * 作成済記事リポジトリの具象クラス
+ *
+ */
+@Repository
+class ArticleRepositoryImpl : ArticleRepository
